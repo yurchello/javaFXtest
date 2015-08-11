30c503c790d7088391ee6af3c9c55e5f69b68a06
mezentsev.y <mezentsev.y@dbbest.com>
2015-08-11 20:09:05 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller  extends Button{
+
+
+}
